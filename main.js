6ff43785410ef92a7bb89d229a9f831390cdb265
26Aclark <123163982+26Aclark@users.noreply.github.com>
2023-02-03 16:08:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// main counter
+let counter = 0;
+  
+document.getElementById('main_button').onclick = () => {
+    counter = counter + 10;
+    //
+    document.getElementById('boxes').innerText = counter;
+  };
+       document.getElementById('upgrades_button').onClick ={ "document.getElementById('hideaway').style.display='block';">Reveal hidden content
+       onClick="document.getElementById('hideaway').innerHTML='new html goes here';">Update the HTML};
