4da0397af14d1d52c2ef9b586c87619487e7fc1b
26Aclark <123163982+26Aclark@users.noreply.github.com>
2023-02-03 21:35:30 -0500

    temp till fix

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,5 @@ document.getElementById('main_button').onclick = () => {
     //
     document.getElementById('boxes').innerText = counter;
   };
-       document.getElementById('upgrades_button').onClick ={ "document.getElementById('hideaway').style.display='block';">Reveal hidden content
-       onClick="document.getElementById('hideaway').innerHTML='new html goes here';">Update the HTML};
+       //document.getElementById('upgrades_button').onClick ={ "document.getElementById('hideaway').style.display='block';">Reveal hidden content
+       //onClick="document.getElementById('hideaway').innerHTML='new html goes here';">Update the HTML};
